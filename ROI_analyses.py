67e888ef7ebbd37957c13047ecafc3652c1215b1
ltuominen <rhamati95@gmail.com>
2020-05-12 16:57:35 -0400

    added script descriptors and condensed script

--- ROI_analyses.py
+++ ROI_analyses.py
@@ -1,108 +1,121 @@
 #!/usr/bin/env python
-# coding: utf-8
-
-# In[10]:
-
-
 #! /home/lauri/anaconda2/bin/python
 
 #load libraries 
 import os
 import nibabel as nb
 import numpy as np
-from nilearn import plotting
-import matplotlib as plt
 import pandas as pd
 
-
-# In[125]:
-
-
-def main():
-    # set paths 
-    # load mri.2mm/aseg.mgz
-    ## set-up environment
+class AtlasInfo():
+    """ This object is just a container for all ROI atlas related info """
+    
     SUBJECTS_DIR = os.environ['SUBJECTS_DIR']
-    ## load mask file using nibabel
     maskfile = SUBJECTS_DIR + '/fsaverage/mri.2mm/aseg.mgz'
-    mask = nb.load(maskfile).get_data()
-    print('The dimensions of the mask are:') 
-    print(mask.shape)
-    
-    # roi definitions
+    mask = nb.load(maskfile).get_fdata()
     rois = {'amy': (18,54), 'cau': (11,50), 'hip': (17,53), 'pal': (13,52), 'put': (12,51), 
-             'tha' : (10,49),'vst':(26,58)} 
-    roiss = rois.keys()
-    print(roiss)
+             'tha' : (10,49),'vst':(26,58)}
     
-    # subjects
-    subjects = []
-    root = '/group/tuominen/EmoSal/subjects/'
-    files = os.listdir(root)
-    for f in files:
-        if f.startswith('AVL'):
-            subjects.append(f)
-    print('Analyzing data from:')
-    subjects.sort()
-    print(subjects)
-    
-    # conditions 
-    conditions=['csm','csp']
-    
-    #create a pandas dataframe with ID + group + ROIs as column names
-    columns = ['ID', 'Status',
-               (str(roiss[0] + '_csp')),(str(roiss[1]) + '_csp'),
-          (str(roiss[2]) + '_csp'), (str(roiss[3]) + '_csp'),
-          (str(roiss[4]) + '_csp'), (str(roiss[5]) + '_csp'),
-          (str(roiss[6]) + '_csp'), (str(roiss[0] + '_csm')),(str(roiss[1]) + '_csm'),
-          (str(roiss[2]) + '_csm'), (str(roiss[3]) + '_csm'),
-          (str(roiss[4]) + '_csm'), (str(roiss[5]) + '_csm'),
-          (str(roiss[6]) + '_csm')]
+    roisIdx = {}
+    for k in rois.keys():
+        roisIdx[('lh_'+ k)] = np.where(mask==rois[k][0])
+        roisIdx[('rh_'+ k)] = np.where(mask==rois[k][1])
+        
+    roisCombined = {}
+    for k in rois.keys():
+        roisCombined[k] = np.where( (mask==rois[k][0]) | (mask==rois[k][1]))   
 
+    def __init__(self):
+        self.rois = self.rois
+        self.roisCombined = self.roisCombined
+        self.roinames = list(self.rois.keys())
+        self.roisIdx = self.roisIdx
+
+def findSubjects(folder, StartPattern):    
+    """This function will find subjects from the folder, based on naming pattern and the sort the subjects """
+    subjects = []
+    files = os.listdir(folder)
+    for f in files:
+        if f.startswith(StartPattern):
+            subjects.append(f)
+            
+    subjects.sort()
+    return subjects
+
+def CreateDF(atlas):
+    """ This function will create a PD dataframe with ID + group + ROIs as column names"""
+    cspCol = [r + '_csp' for r in atlas.roinames]
+    csmCol = [r + '_csm' for r in atlas.roinames]
+    
+    columns = ['ID', 'Status'] + cspCol + csmCol
     df_ = pd.DataFrame( columns=columns)
     df_ = df_.reindex(sorted(df_.columns), axis=1)
+    return df_ 
+
+def fillDF(subjects, folder, atlas, df_) :
+    """ This function will loop over subjects, extract ROI values and fill out the DF
+    Needs a list of subjects, the folder where they are located and a DF as inputs """
     
-    #loop over subjects
     for s in subjects:
-        store = [] 
-        if int(s[-3:]) < 100:
-            status='control'
-        else:
-            status='FDR'
-        g= [s, status]  
-        check = os.path.join('/group/tuominen/EmoSal/subjects/' + s + '/bold/fc.mni305/csp/cespct.nii.gz')
-        if os.path.isfile(check):
-            print('file exists for:')
-            print(s)
-    # create a new row for the subject, you may want to test if that row already exists, if so skip that subject
-        # loop over ROIs
-            for k in rois.keys():
-                lhrh = rois[k]
-                lhidx = np.where(mask==lhrh[0])
-                rhidx = np.where(mask==lhrh[1])
-        # loop over conditions          
-                for c in conditions:
-                    filename = os.path.join(root + s + '/bold/fc.mni305/' + c +'/cespct.nii.gz')
-                    fMRI = nb.load(filename).get_data()
-                    datapoint = np.mean( np.concatenate( ( fMRI[lhidx], fMRI[rhidx] ) ) )
-                    store.append(datapoint)
-                    print(store)
-# put this data point to the right location in the dataframe
-                    #for i in store:
-                        #if len(store) >= 14:
-            d = g + [str(i) for i in store]
-            swdf=pd.DataFrame(data=[d] ,columns=columns)
-            frames = [df_, swdf]
-            df_ = pd.concat(frames, sort=False)
-            store = []
+        check = os.path.join(folder + s + '/bold/fc.mni305/csp/cespct.nii.gz')
+        if not os.path.isfile(check):
+            return
+        
+        swdf = extractData(s, folder, atlas)
+        frames = [df_, swdf]
+        df_ = pd.concat(frames, sort=False)
+        frames = [df_, swdf]
+        df_ = pd.concat(frames, sort=False)
+        store = []
                         #else: 
                             #continue
-    # save pandas dataframe as a csv
+   
+    return df_
+
+def extractData(s, folder, atlas):
+    """ This function will load subject's fMRI data and extract mean values from each ROI """
+    store = [] 
+    conditions = ['csp', 'csm']
+    for k in atlas.roinames():         
+        for c in conditions:
+            filename = os.path.join(folder + s + '/bold/fc.mni305/' + c +'/cespct.nii.gz')
+            fMRI = nb.load(filename).get_fdata()
+            datapoint = np.mean( fMRI[atlas.roisCombined[k]] )
+            store.append(datapoint)
+
+    if int(s[-3:]) < 100:
+        status='control'
+    else:
+        status='FDR'
+        
+    d = [s, status] + [str(i) for i in store]
+    swdf=pd.DataFrame(data=[d] )
+    return swdf
+
+def writeDF(df_, dname, fname):
+    """ This function will save the dataframe in the location defined by dname and fname"""
     date = pd.to_datetime('today').strftime("%d_%m_%Y_")
-    date_stamp = os.path.join('/group/tuominen/EmoSal/ROI/' + date + 'ROI_analysis.csv')
-    print(date_stamp)
+    date_stamp = os.path.join(dname + date + fname)
     df_.to_csv((date_stamp), index = True, header=True)          
 
+def main():
+    ## This stuff is hardcoded, change when needed
+    folder = '/group/tuominen/EmoSal/subjects/'
+    StartPattern = 'AVL'
+    atlas = AtlasInfo()
+    dname = '/group/tuominen/EmoSal/ROI/'
+    fname = 'ROI_analysis.csv'
+    
+    # get subjects 
+    subjects = findSubjects(folder, StartPattern)
+    # create a dataframe
+    df_ = CreateDF()
+    # extract ROI info from each subject and put it into the dataframe
+    data = fillDF(subjects, folder, atlas, df_)
+    # save dataframe
+    writeDF(data, dname, fname)
+    
+    
 if __name__ == "__main__":
     # execute only if run as a script
     main()
