c17aaa1ada9b52a3e6473bf53c840f4d85b02562
ramroomh <rhamati95@gmail.com>
2021-04-09 15:41:07 -0400

    changed threshold

--- cluster_sum_surf.sh
+++ cluster_sum_surf.sh
@@ -1,6 +1,6 @@
 hemi='lh rh'
 for h in ${hemi[@]}; do
-  path=/group/tuominen/EmoSal/group/fc.$h/csp_v_csm/AVL_GLM.wls/osgm/
-  mri_surfcluster --in $path/sig.nii.gz --minarea 150 --thmin 2 --hemi rh \
+  path=/group/tuominen/EmoSal/group/fc.$h/csp_v_csm/AVL_GLM.wls/osgm
+  mri_surfcluster --in $path/sig.nii.gz --minarea 500 --thmin 1.3 --thmax 3 --hemi $h \
   --subject fsaverage --sign abs --surf inflated --oannot $path/$h.cluster.sig.annot
 done
