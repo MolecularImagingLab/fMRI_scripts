6257679130698e47fd2a7203c9e4610b57469f22
ramroomh <rhamati95@gmail.com>
2021-04-09 14:56:48 -0400

    modified for parametric analysis

--- mkcontrast.sh
+++ mkcontrast.sh
@@ -1,12 +1,13 @@
-mkcontrast-sess -analysis ../mkanalysis/fc.lh -contrast csp_v_csm -a 1 -c 2
-mkcontrast-sess -analysis ../mkanalysis/fc.rh -contrast csp_v_csm -a 1 -c 2
-mkcontrast-sess -analysis ../mkanalysis/fc.mni305 -contrast csp_v_csm -a 1 -c 2
-mkcontrast-sess -analysis ../mkanalysis/fc.lh -contrast csp -a 1
-mkcontrast-sess -analysis ../mkanalysis/fc.rh -contrast csp -a 1
-mkcontrast-sess -analysis ../mkanalysis/fc.mni305 -contrast csp -a 1
-mkcontrast-sess -analysis ../mkanalysis/fc.lh -contrast csm -a 2
-mkcontrast-sess -analysis ../mkanalysis/fc.rh -contrast csm -a 2
-mkcontrast-sess -analysis ../mkanalysis/fc.mni305 -contrast csm -a 2
-mkcontrast-sess -analysis ../mkanalysis/fc.lh -contrast csps -a 3
-mkcontrast-sess -analysis ../mkanalysis/fc.rh -contrast csps -a 3
-mkcontrast-sess -analysis ../mkanalysis/fc.mni305 -contrast csps -a 3
+space='fc.lh fc.rh fc.mni305'
+for s in ${space[@]}; do
+  # Slope CSP versus slope CSM
+  mkcontrast-sess -analysis ../mkanalysis/$s -contrast csp_v_csm_slope -a 2 -c 4
+  # Offset CSP versus offset CSM
+  mkcontrast-sess -analysis ../mkanalysis/$s -contrast csp_v_csm_offset -a 1 -c 3
+  # CSM
+  mkcontrast-sess -analysis ../mkanalysis/$s -contrast csp_slope -a 2
+  mkcontrast-sess -analysis ../mkanalysis/$s -contrast csp_offset -a 1
+  # CSP
+  mkcontrast-sess -analysis ../mkanalysis/$s -contrast csm_slope -a 4
+  mkcontrast-sess -analysis ../mkanalysis/$s -contrast csm_offset -a 3
+done
