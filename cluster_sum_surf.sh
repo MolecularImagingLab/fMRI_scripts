0e451862b95be9f3da33ea77cd785692678bcc75
ramroomh <rhamati95@gmail.com>
2021-04-13 17:14:19 -0400

    lowered minarea

--- cluster_sum_surf.sh
+++ cluster_sum_surf.sh
@@ -1,6 +1,6 @@
 hemi='lh rh'
 for h in ${hemi[@]}; do
-  path=/group/tuominen/EmoSal/group/fc.$h/csp_v_csm/AVL_GLM.wls/osgm
-  mri_surfcluster --in $path/sig.nii.gz --minarea 500 --thmin 1.3 --thmax 3 --hemi $h \
+  path=/group/tuominen/EmoSal/group/fc.$h/csp_v_csm/pr001/AVL_GLM.wls/osgm
+  mri_surfcluster --in $path/sig.nii.gz --minarea 200 --thmin 1.3 --thmax 3 --hemi $h \
   --subject fsaverage --sign abs --surf inflated --oannot $path/$h.cluster.sig.annot
 done
